6d7bb644088ed1fd9347897589f9199a0823f547
Tom Yasinchuk <tomyasinchuk@Toms-MacBook-Air.local>
2023-12-03 21:33:20 -0800

    formatted stock data into dataframe

--- client.py
+++ client.py
@@ -3,6 +3,10 @@ import config
 import json
 from typing import cast
 from urllib3 import HTTPResponse
+from plotly import graph_objects as graph
+import pandas as pd
+import numpy as np
+
 client = RESTClient(config.API_KEY)
 
 aggs = cast(
@@ -13,17 +17,27 @@ aggs = cast(
         "day",
         "2023-01-01",
         "2023-03-01",
-        raw = True
-    ),
-
+        raw=True
+    )
 )
-
 data = json.loads(aggs.data)
+results = data.get('results', [])
+close_list = [bar['c'] for bar in results]
+open_list = [bar['o'] for bar in results]
+high_list = [bar['h'] for bar in results]
+low_list = [bar['l'] for bar in results]
+date_list = [bar['t'] for bar in results]
 
-print(data)
+date_list = [pd.to_datetime(date, unit='ms', origin='unix').strftime('%Y-%m-%d') for date in date_list]
 
 
-
-
-
+df = pd.DataFrame({
+    "Date": date_list,
+    'Open': open_list,
+    'High': high_list,
+    'Low': low_list,
+    'Close': close_list
+})
+pd.set_option('display.max_rows', None)
+print(df)
 
